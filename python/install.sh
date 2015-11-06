88caf8c5554e3d7b4bef09e6935a409aa6f82ef2
Jackie <wangranche@gmail.com>
2015-11-07 01:17:42 +0800

    Add pip lists

--- python/install.sh
+++ python/install.sh
@@ -1,14 +1,41 @@
 #!/bin/sh
 
 plugins=(
-    virtualenv
-    powerline-status
+    appnope
     autopep8
-    flake8
+    click
+    decorator
     docopt
+    flake8
+    Glances
+    gnureadline
+    ipython
+    ipython-genutils
+    isitup
+    linecache2
+    mccabe
+    nose
+    numpy
+    path.py
+    pep8
+    pexpect
+    pickleshare
+    pip
+    powerline-status
+    psutil
+    ptyprocess
+    pyflakes
     pytz
     requests
+    setuptools
+    simplegeneric
+    six
+    traceback2
+    traitlets
     unittest2
+    vboxapi
+    virtualenv
+    wheel
 )
 
 pip install --upgrade pip
